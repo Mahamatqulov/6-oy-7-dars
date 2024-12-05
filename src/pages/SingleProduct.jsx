a41a32368dfd063ea691af61423c7490d157396f
bahrom <baxrom@email.com>
2024-12-05 14:01:47 +0500

    M-market ready

---
+++ src/pages/SingleProduct.jsx
@@ -0,0 +1,16 @@
+import { useLoaderData } from "react-router-dom";
+import { axiosInstance } from "../utils/axiosInstance";
+
+export const loader = async ({ params }) => {
+  const req = await axiosInstance.get("/products/" + params.id);
+  const product = req.data;
+  return { product };
+};
+
+function SingleProduct() {
+  const { product } = useLoaderData();
+  console.log(product);
+  return <div>{product.title}</div>;
+}
+
+export default SingleProduct;
